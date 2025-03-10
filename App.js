20f6ac591faeba88ce3bff4fe16210c9993fb658
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-10 11:40:41 -0600

    Working with core components. (Simple usage change of the core components)

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Another piece of text!</Text>
       <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Button title='Tap me!' />
     </View>
   );
 }
